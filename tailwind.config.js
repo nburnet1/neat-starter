53fcdb88f7a454bd502f6159121d60d45a8b476a
nburnet1 <noah.burnette@intellicintegration.com>
2026-07-06 17:39:08 -0400

    revamped site

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 module.exports = {
-  content: ["./**/*.html"],
+  content: ["./src/**/*.html"],
   theme: {
     container: {
       center: true,
     },
     extend: {
-      colors: {},
+      colors: {
+        brand: {
+          pine: "#002715",
+          ink: "#171311",
+          bark: "#241a16",
+          hide: "#3b2b23",
+          brass: "#a66d2c",
+          rust: "#7f4d1d",
+          bone: "#f1e7d9",
+          sand: "#dcc8ae",
+          smoke: "#6d6258",
+        },
+      },
     },
   },
   variants: {},
